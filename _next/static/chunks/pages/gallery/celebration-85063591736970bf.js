d3c584278ff8a2a6048af2abc68fe8869dd10e22
sezhian <sezhian@mindwaveventures.com>
2024-09-05 16:23:53 +0530

    Updates

---
+++ _next/static/chunks/pages/gallery/celebration-85063591736970bf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[224],{1841:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/gallery/celebration",function(){return u(6730)}])}},function(n){n.O(0,[959,504,730,774,888,179],function(){return n(n.s=1841)}),_N_E=n.O()}]);
